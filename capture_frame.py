f890ff16d3c0ae22f9badd6f3a750c481825257c
Yuhao Mao <maoyuhao92@gmail.com>
2019-07-23 16:39:33 -0400

    换了frame大小

--- capture_frame.py
+++ capture_frame.py
@@ -1,17 +1,19 @@
 import cv2
 
 cap=cv2.VideoCapture(0)
-i=0
+i=200
 while(1):
     ret ,frame = cap.read()
     k=cv2.waitKey(1)
-    cropImg = frame[20:400, 20:400]
+    cropImg = frame[200:400, 300:500]
     if k==27:
         break
     elif k==ord('s'):
         cv2.imwrite('/Users/yuhaomao/Desktop/lu_dianzan/'+str(i)+'.jpg',cropImg)
         i+=1
-    cv2.rectangle(frame, (20,20), (400, 400), (0, 255, 0), 1)
+    # cv2.rectangle(frame, (20,20), (400, 400), (0, 255, 0), 1)
+    cv2.rectangle(frame, (300, 200), (500, 400), (0, 255, 0), 1)
+    # frame = cv2.flip(frame, 1)
     cv2.imshow("capture", frame)
 cap.release()
 cv2.destroyAllWindows()
